9ad8336629cc6539eec59546d29b973ab7a8a9db
iobitt <sashabratm@gmail.com>
2021-07-09 13:08:18 +0300

    changed request method for '/api/get_token'

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'account/registration', action: :registration, controller: 'account'
   get 'account/logout', action: :logout, controller: 'account'
   get '/', action: :index, controller: :products
-  get '/api/get_token', action: :get_token, controller: :account
+  post '/api/get_token', action: :get_token, controller: :account
 
   resources :products
 end
