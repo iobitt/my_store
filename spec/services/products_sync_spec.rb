df9a4169dbb61ba673e5991b626a36e3b76ca788
iobitt <sashabratm@gmail.com>
2021-07-14 18:50:22 +0300

    update products_sync_spec

--- spec/services/products_sync_spec.rb
+++ spec/services/products_sync_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 
 describe ProductsPuller do
 
+  before :each do
+    @user = Manager.create!(name: "ivan", password: "12891289")
+    @products_puller = ProductsPuller.new(@user)
+  end
+
   it "проверяет создание товаров" do
-    user = Manager.create!(name: "ivan", password: "12891289")
-    products_puller = ProductsPuller.new(user)
     data = {
       "products": [
         {
@@ -34,37 +37,31 @@ describe ProductsPuller do
         }
       ]
     }
+    products = ProductMirror.where :user_id => @user.id
 
-    expect(ProductMirror.count).to eq(0)
-    products = ProductMirror.where :user_id => user.id
     data = data.deep_transform_keys{ |key| key.to_s }
-    products_puller.send :create_or_update_products, data, products, [1, 2, 3]
+    @products_puller.send :create_or_update_products, data, products, [1, 2, 3]
+
     expect(ProductMirror.count).to eq(3)
   end
 
   it "проверяет удаление товаров" do
-    user = Manager.create!(name: "ivan", password: "12891289")
-    products_puller = ProductsPuller.new(user)
+    ProductMirror.create!({external_id: 1, name: "Телефон", price: 3500, quantity: 10, user_id: @user.id})
+    ProductMirror.create!({external_id: 2, name: "Телевизор", price: 3500, quantity: 10, user_id: @user.id})
+    ProductMirror.create!({external_id: 3, name: "Планшет", price: 3500, quantity: 10, user_id: @user.id})
 
-    ProductMirror.create!({external_id: 1, name: "Телефон", price: 3500, quantity: 10, user_id: user.id})
-    ProductMirror.create!({external_id: 2, name: "Телевизор", price: 3500, quantity: 10, user_id: user.id})
-    ProductMirror.create!({external_id: 3, name: "Планшет", price: 3500, quantity: 10, user_id: user.id})
+    products = ProductMirror.where :user_id => @user.id
 
-    products = ProductMirror.where :user_id => user.id
-    product = ProductMirror.find_by_external_id(2)
-    expect(product.is_archived).to eq(false)
-    products_puller.send :delete_products, products, [2]
+    @products_puller.send :delete_products, products, [2]
+
     product = ProductMirror.find_by_external_id(2)
     expect(product.is_archived).to eq(true)
   end
 
   it "проверяет изменение товаров" do
-    user = Manager.create!(name: "ivan", password: "12891289")
-    products_puller = ProductsPuller.new(user)
-
-    ProductMirror.create!({external_id: 1, name: "Телефон", price: 3500, quantity: 10, user_id: user.id})
-    ProductMirror.create!({external_id: 2, name: "Телевизор", price: 30, quantity: 180, user_id: user.id})
-    ProductMirror.create!({external_id: 3, name: "Планшет", price: 80, quantity: 300, user_id: user.id})
+    ProductMirror.create!({external_id: 1, name: "Телефон", price: 3500, quantity: 10, user_id: @user.id})
+    ProductMirror.create!({external_id: 2, name: "Телевизор", price: 30, quantity: 180, user_id: @user.id})
+    ProductMirror.create!({external_id: 3, name: "Планшет", price: 80, quantity: 300, user_id: @user.id})
 
     data = {
       "products": [
@@ -95,9 +92,11 @@ describe ProductsPuller do
       ]
     }
 
-    products = ProductMirror.where :user_id => user.id
+    products = ProductMirror.where :user_id => @user.id
     data = data.deep_transform_keys{ |key| key.to_s }
-    products_puller.send :create_or_update_products, data, products, []
+
+    @products_puller.send :create_or_update_products, data, products, []
+
     product = ProductMirror.find_by_external_id(1)
     expect(product.name).to eq("Телефон 2")
     expect(product.price).to eq(1700)
@@ -109,9 +108,12 @@ end
 
 describe ProductsSyncer do
 
-  it "проверяет создание товаров" do
-    before_sync_datetime = DateTime.now
+  before :each do
+    @before_sync_datetime = DateTime.now
     Manager.create!(id: 5, name: "ivan", password: "12891289")
+  end
+
+  it "проверяет создание товаров" do
     ProductMirror.create!({external_id: 1, name: "Телефон", price: 3500, quantity: 10, user_id: 5,
                            external_created_at: DateTime.new(2021, 7, 14, 4, 5, 6),
                            external_updated_at: DateTime.new(2021, 7, 14, 4, 5, 6)
@@ -127,14 +129,11 @@ describe ProductsSyncer do
 
     expect(ProductMirror.count).to eq(3)
     expect(Product.count).to eq(0)
-    ProductsSyncer.new(before_sync_datetime).call
+    ProductsSyncer.new(@before_sync_datetime).call
     expect(Product.count).to eq(3)
   end
 
   it "проверяет изменение товаров" do
-    before_sync_datetime = DateTime.now
-    Manager.create!(id: 5, name: "ivan", password: "12891289")
-
     Product.create!({id: 1, name: "Телефон", price: 3500, quantity: 10, user_id: 5 })
     Product.create!({id: 2, name: "Телевизор", price: 3500, quantity: 10, user_id: 5 })
     Product.create!({id: 3, name: "Планшет", price: 3500, quantity: 10, user_id: 5 })
@@ -152,7 +151,7 @@ describe ProductsSyncer do
                            external_updated_at: DateTime.new(2021, 7, 14, 4, 5, 8)
                           })
 
-    ProductsSyncer.new(before_sync_datetime).call
+    ProductsSyncer.new(@before_sync_datetime).call
 
     product3 = Product.find(3)
     expect(product3.name).to eq("Планшет большой")
@@ -161,9 +160,6 @@ describe ProductsSyncer do
   end
 
   it "проверяет удаление товаров" do
-    before_sync_datetime = DateTime.now
-    Manager.create!(id: 5, name: "ivan", password: "12891289")
-
     Product.create!({id: 1, name: "Телефон", price: 3500, quantity: 10, user_id: 5 })
     Product.create!({id: 2, name: "Телевизор", price: 3500, quantity: 10, user_id: 5 })
     Product.create!({id: 3, name: "Планшет", price: 3500, quantity: 10, user_id: 5 })
@@ -181,7 +177,7 @@ describe ProductsSyncer do
                            external_updated_at: DateTime.new(2021, 7, 14, 4, 5, 8), is_archived: true
                           })
 
-    ProductsSyncer.new(before_sync_datetime).call
+    ProductsSyncer.new(@before_sync_datetime).call
     expect(Product.exists?(3)).to eq(false)
   end
 end
